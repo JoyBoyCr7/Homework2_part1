ab89ac12b2721cfbb4c1f1dbe81cb24a88244e3e
Ronard Nyongkah <ronardnyongkah@Ronards-MacBook-Air.local>
2023-05-02 00:02:47 -0500

    finished loop practice

---
+++ loops.js
@@ -0,0 +1,32 @@
+// Create a loop that logs the numbers from 0-99 (ascending)
+
+for (let i = 0;i < 100; i++){
+    console.log(i)
+}
+
+// Create a loop that logs the numbers from 99-0 (descending)
+
+for (let i = 99;i > -1; i--){
+    console.log(i)
+}
+
+// Create a loop that logs the EVEN numbers from 0-98 (ascending)
+
+for (let i = 0;i < 100; i++){
+    if (i % 2  == 0){
+    console.log(i)}
+}
+
+// Create a loop that logs the EVEN numbers from 98-0 (descending)
+
+for (let i = 98;i > -1; i--){
+    if (i % 2  == 0){
+    console.log(i)}
+}
+
+// Create a loop that logs the ODD numbers from 0-99 (ascending)
+
+for (let i = 0;i < 100; i++){
+    if (i % 2  != 0){
+    console.log(i)}
+}
